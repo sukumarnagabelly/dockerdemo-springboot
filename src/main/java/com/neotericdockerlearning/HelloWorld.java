36c3b0c5eb624f1ff829e6427b01fa2322016b78
SUKUMAR <sukumarnagabelly@gmail.com>
2024-10-06 10:48:46 +0530

    gi

---
+++ src/main/java/com/neotericdockerlearning/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.neotericdockerlearning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloWorld {
+    @GetMapping(value = "api/hello",consumes = "application/json",produces = "application/json")
+    public String getHelloWorld (){
+        System.out.println("hello");
+        return "hello wolrd";
+    }
+}
